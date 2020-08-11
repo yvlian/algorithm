ccaeee9e83089063c111d200580ee15307b1a953
yvlian <2509123408@qq.com>
2020-08-11 16:19:36 +0800

    sql

--- 查找与排序/堆排序.py
+++ 查找与排序/堆排序.py
@@ -1,25 +1,23 @@
-def adjust(nums, i):
-    lson, rson = 2 * i + 1, 2 * i + 2
-    n = len(nums)
-    max_idx = i
-    if lson < n and nums[lson] > nums[max_idx]:
-        max_idx = lson
-    if rson < n and nums[rson] > nums[max_idx]:
-        max_idx = rson
-    if max_idx != i:
-        nums[i], nums[max_idx] = nums[max_idx], nums[i]
-        adjust(nums, max_idx)
-    return nums
+def heapSort(arr,n,i):
+    miniest,l,r = i,2*i+1,2*i+2
+    if l<n and arr[l] < arr[miniest]:
+        miniest = l
+    if r<n and arr[r] < arr[miniest]:
+        miniest = r
+    if miniest != i:
+        arr[miniest],arr[i] = arr[i],arr[miniest]
+        heapSort(arr,n,miniest)
+def minHeapSort(arr):
+    n = len(arr)
+    for i in range(n-1,-1,-1):
+        heapSort(arr,n,i)
+    for i in range(n-1,0,-1):
+        arr[i],arr[0] = arr[0],arr[i]
+        heapSort(arr,i,0)
 
-
-def heap_sort(nums):
-    n = len(nums)
-    for i in range(n - 1, -1, -1):
-        nums = adjust(nums, i)
-    for i in range(n - 1, -1, -1):
-        nums[i], nums[0] = nums[0], nums[i]
-        nums[:i] = adjust(nums[:i], 0)
-    print(nums)
-    return nums
-x = [6,  1 , 2, 7,  9 , 3 , 4  ,5 ,10 , 8]
-heap_sort(x)
+arr = [10,46,22,81,91,60,31,30,77,96,13,20,65,35,65]
+minHeapSort(arr)
+n = len(arr)
+print("排序后")
+for i in range(n):
+    print("%d" % arr[i])
